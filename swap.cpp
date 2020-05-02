3eb66e28cdc38d2c284863d88c46bc20c9c84bc8
Mohit Yadav <39982615+mohit22yadav@users.noreply.github.com>
2020-05-02 11:20:47 +0530

    swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int a , int b)
+{
+	int temp=a;
+	a=b;
+	b=temp;
+	cout<<"A :  "<<X<<endl<<"B :  "<<endl;
+}
+
+
+
+int main()
+{
+	int x=10, y=6;
+
+	swap(x,y);
+	cout<<"X :  "<<X<<endl<<"Y :  "<<endl;
+    
+    return 0;
+
+}
